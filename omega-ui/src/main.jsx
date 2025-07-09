5feedbf43f37c1b52f39d84a2bcf5884f75430cb
Zyroun <eaonframework@gmail.com>
2025-07-09 09:46:30 +0100

    Update main.jsx

--- omega-ui/src/main.jsx
+++ omega-ui/src/main.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { OmegaProvider } from './context/OmegaContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <OmegaProvider>
+      <Router>
+        <App />
+      </Router>
+    </OmegaProvider>
   </React.StrictMode>
 );
 
