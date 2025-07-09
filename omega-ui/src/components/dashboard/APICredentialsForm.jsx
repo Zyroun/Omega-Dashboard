35f6b4c9ae40bbe49ac2de43116b8dcfe0e6c1db
Zyroun <eaonframework@gmail.com>
2025-07-09 21:44:20 +0100

    Create APICredentialsForm.jsx

---
+++ omega-ui/src/components/dashboard/APICredentialsForm.jsx
@@ -0,0 +1,16 @@
+const defaultCredentials = {
+  shopify: {
+    apiKey: '',
+    apiSecret: '',
+    accessToken: '',
+    enabled: false,
+  },
+  tiktok: {
+    apiKey: '',
+    enabled: false,
+  },
+  ebay: {
+    apiKey: '',
+    enabled: false,
+  },
+};
